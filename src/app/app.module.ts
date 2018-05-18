c3e9a27624ab9c8b963a5ab691ce3414f282f350
Peszi <mat_mos@wp.pl>
2018-05-18 08:10:26 +0200

    maps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {AgmCoreModule} from '@agm/core';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAXra6wL8hINthUcIzU-DS5CaL2ei5Eu-A'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]

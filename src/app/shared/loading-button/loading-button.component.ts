65d06a230a874a94f744139d4d7a06752f2342ee
Peszi <mat_mos@wp.pl>
2018-05-22 15:34:02 +0200

    signing up refactoring, fixes

---
+++ src/app/shared/loading-button/loading-button.component.ts
@@ -0,0 +1,31 @@
+import {Component, ElementRef, Input, OnInit, ViewEncapsulation} from '@angular/core';
+import {BACKGROUND_SCROLL, SHOW_AND_HIDE} from '../animations/background-scrolling.animation';
+
+@Component({
+  selector: 'app-loading-button',
+  template: '<ngb-alert [dismissible]="false" [@showElement]="visibilityMessage" (@showElement.done)="onEnd()" style="opacity: 0"><strong>Alert!</strong> {{ errorMessage }}</ngb-alert>',
+  // styleUrls: ['./loading-button.component.css'],
+  animations: [ SHOW_AND_HIDE ]
+})
+export class LoadingButtonComponent implements OnInit {
+
+  visibilityMessage: String = 'hide';
+  errorMessage: any;
+
+  constructor() {}
+
+  ngOnInit() {
+
+    // [@loading]="loadingScroll" (@loading.done)="onLoadingEnd($event)"
+  }
+
+  onEnd() {
+    this.visibilityMessage = 'hide';
+  }
+
+  showMessage(message: string) {
+    this.errorMessage = message;
+    this.visibilityMessage = 'show';
+  }
+
+}
